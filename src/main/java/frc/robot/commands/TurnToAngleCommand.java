98caa041123c342074ff671aa832cd8f3220d15e
bmongar <bmongar@keyholesoftware.com>
2021-12-08 17:12:36 -0600

    gains place holders
    Added Gains for translation .  X and  Y translation should be the same
    Renamed TurnGains to ThetaGains to be more consistent with naming of WPILIB Swerve classes

--- src/main/java/frc/robot/commands/TurnToAngleCommand.java
+++ src/main/java/frc/robot/commands/TurnToAngleCommand.java
@@ -5,32 +5,23 @@ import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
-import static frc.robot.Constants.DrivetrainConstants.TurnGains.*;
+import static frc.robot.Constants.DrivetrainConstants.ThetaGains.*;
 
 public class TurnToAngleCommand extends PIDCommand {
-  public TurnToAngleCommand(DrivetrainSubsystem driveTrain, double angleRadians) {
-    super(
-        // The controller that the command will use
-        new PIDController(kP, 
-                          kI,
-                          kD),
-        driveTrain::getHeading,
-        angleRadians,
-        // Pipe the output to the turning controls
-        output -> driveTrain.drive(                
-            ChassisSpeeds.fromFieldRelativeSpeeds(
-            0,
-            0,
-            output,
-            driveTrain.getGyroscopeRotation()
-        )));
-    getController().setTolerance(kTurnToleranceRad , kTurnRateToleranceRadPerS);
-  }
+    public TurnToAngleCommand(DrivetrainSubsystem driveTrain, double angleRadians) {
+        super(
+                // The controller that the command will use
+                new PIDController(kP, kI, kD), driveTrain::getHeading, angleRadians,
+                // Pipe the output to the turning controls
+                output -> driveTrain
+                        .drive(ChassisSpeeds.fromFieldRelativeSpeeds(0, 0, output, driveTrain.getGyroscopeRotation())));
+        getController().setTolerance(kTurnToleranceRad, kTurnRateToleranceRadPerS);
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 
 }
