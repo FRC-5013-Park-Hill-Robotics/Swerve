efb0ad296085913c20f1a429c9790d0fd558b40e
bmongar <bradmongar@gmail.com>
2021-12-24 16:14:18 -0600

    Offsets and pigeon fix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,10 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import static frc.robot.Constants.*;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,7 +20,6 @@ public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private RobotContainer m_robotContainer;
-    private PowerDistributionPanel m_pdp = new PowerDistributionPanel(PDP_ID);
 
     /**
      * This function is run when the robot is first started up and should be used
